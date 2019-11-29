3067aa692b089ade51e22b6abbefcf50c92b5ac4
Juss <soerobertjuss@gmail.com>
2019-11-30 01:10:04 +0200

    organiseeritud, kood lühemaks tehtud

---
+++ js/menu.js
@@ -0,0 +1,8 @@
+// Script to open and close sidebar https://www.w3schools.com/w3css/tryit.asp?filename=tryw3css_templates_food_blog&stacked=h
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+}
+ 
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
---
+++ js/slideshow.js
@@ -0,0 +1,45 @@
+<!---https://www.w3schools.com/w3css/w3css_slideshow.asp--->
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-white", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " w3-white";
+}
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}
+  var rand = Math.floor((Math.random() * x.length));
+  x[rand].style.display = "block";  
+  setTimeout(carousel, 5500);    
+}
+
